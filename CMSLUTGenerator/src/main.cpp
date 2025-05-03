7c020c7e79d126937786f01ec5543e5d9e3f850d
garamond13 <98652255+garamond13@users.noreply.github.com>
2025-05-03 12:18:54 +0200

    CMS: Improve CMSLUTGenerator
    -Transform float LUT for CUBE LUT output.
    - Limit LUT size for 8 bit DDS LUT to 256.

--- CMSLUTGenerator/src/main.cpp
+++ CMSLUTGenerator/src/main.cpp
@@ -39,6 +39,20 @@ static auto cube(auto a)
 	return a * a * a;
 }
 
+template<typename T>
+static size_t get_lut_size()
+{
+	// We only expect uint8_t and float.
+	static_assert(std::is_same_v<T, uint8_t> || std::is_same_v<T, float>);
+
+	if (std::is_same_v<T, uint8_t>) {
+		return std::min(g_config.lut_size, 256ull);
+	}
+	else {
+		return g_config.lut_size;
+	}
+}
+
 static cmsHPROFILE cms_create_profile_srgb()
 {
 	const cmsCIExyY whitepoint = { 0.3127, 0.3290, 1.0 };
@@ -77,33 +91,38 @@ static cmsHPROFILE get_custom_dst_profile()
 	return cmsOpenProfileFromFile(g_config.custom_dst_profile.c_str(), "r");
 }
 
-template<typename T, int byte_depth = sizeof(T)>
-static void fill_lut(std::vector<T>& lut)
+static void fill_lut_rgba8(std::vector<uint8_t>& lut_rgba8)
 {
-	// We only expect uint8_t and uint16_t.
-	assert(byte_depth == 1 || byte_depth == 2);
-	
-	size_t max;
-	if (byte_depth == 1) {
-		max = 255;
-	}
-	else {
-		max = 65535;
-	}
+	const size_t lut_size = get_lut_size<uint8_t>();
 	size_t i = 0;
-	for (size_t b = 0; b < g_config.lut_size; ++b) {
-		for (size_t g = 0; g < g_config.lut_size; ++g) {
-			for (size_t r = 0; r < g_config.lut_size; ++r) {
-				lut[i++] = r * max / (g_config.lut_size - 1);
-				lut[i++] = g * max / (g_config.lut_size - 1);
-				lut[i++] = b * max / (g_config.lut_size - 1);
-				lut[i++] = max; // Alpha.
+	for (size_t b = 0; b < lut_size; ++b) {
+		for (size_t g = 0; g < lut_size; ++g) {
+			for (size_t r = 0; r < lut_size; ++r) {
+				lut_rgba8[i++] = r * 255 / (lut_size - 1);
+				lut_rgba8[i++] = g * 255 / (lut_size - 1);
+				lut_rgba8[i++] = b * 255 / (lut_size - 1);
+				i++; // Iterate over alpha.
 			}
 		}
 	}
 }
 
-static bool transform_luts(std::vector<uint8_t>& lut8, std::vector<uint16_t>& lut16)
+static void fill_lut_rgb32f(std::vector<float>& lut_rgb32f)
+{
+	const size_t lut_size = get_lut_size<float>();
+	size_t i = 0;
+	for (size_t b = 0; b < lut_size; ++b) {
+		for (size_t g = 0; g < lut_size; ++g) {
+			for (size_t r = 0; r < lut_size; ++r) {
+				lut_rgb32f[i++] = static_cast<float>(r) / static_cast<float>(lut_size - 1);
+				lut_rgb32f[i++] = static_cast<float>(g) / static_cast<float>(lut_size - 1);
+				lut_rgb32f[i++] = static_cast<float>(b) / static_cast<float>(lut_size - 1);
+			}
+		}
+	}
+}
+
+static bool transform_luts(std::vector<uint8_t>& lut_rgba8, std::vector<float>& lut_rgb32f)
 {
 	cmsUInt32Number flags = cmsFLAGS_NOCACHE | cmsFLAGS_HIGHRESPRECALC | cmsFLAGS_NOOPTIMIZE;
 	if (g_config.bpc) {
@@ -117,52 +136,52 @@ static bool transform_luts(std::vector<uint8_t>& lut8, std::vector<uint16_t>& lu
 		return false;
 	}
 
-	auto transform8 = cmsCreateTransform(src_profile, TYPE_RGBA_8, dst_profile, TYPE_BGRA_8, g_config.intent, flags);
-	auto transform16 = cmsCreateTransform(src_profile, TYPE_RGBA_16, dst_profile, TYPE_RGBA_16, g_config.intent, flags);
+	auto transform_rgba8 = cmsCreateTransform(src_profile, TYPE_RGBA_8, dst_profile, TYPE_BGRA_8, g_config.intent, flags);
+	auto transform_rgb32f = cmsCreateTransform(src_profile, TYPE_RGB_FLT, dst_profile, TYPE_RGB_FLT, g_config.intent, flags);
 
 	// At this point we don't need profiles anymore.
 	cmsCloseProfile(src_profile);
 	cmsCloseProfile(dst_profile);
 
-	if (!transform8 || !transform16) {
+	if (!transform_rgba8 || !transform_rgb32f) {
 		return false;
 	}
 
 	// Create LUTs.
-	fill_lut(lut8);
-	fill_lut(lut16);
-	cmsDoTransform(transform8, lut8.data(), lut8.data(), cube(g_config.lut_size));
-	cmsDoTransform(transform16, lut16.data(), lut16.data(), cube(g_config.lut_size));
+	fill_lut_rgba8(lut_rgba8);
+	fill_lut_rgb32f(lut_rgb32f);
+	cmsDoTransform(transform_rgba8, lut_rgba8.data(), lut_rgba8.data(), cube(get_lut_size<uint8_t>()));
+	cmsDoTransform(transform_rgb32f, lut_rgb32f.data(), lut_rgb32f.data(), cube(get_lut_size<float>()));
 	
 	// Clean up.
-	cmsDeleteTransform(transform8);
-	cmsDeleteTransform(transform16);
+	cmsDeleteTransform(transform_rgba8);
+	cmsDeleteTransform(transform_rgb32f);
 
 	return true;
 }
 
-static void write_lut_to_cube_file(const std::vector<uint16_t>& lut)
+static void write_lut_to_cube_file(const std::vector<float>& lut)
 {
 	std::ofstream file("CMSLUT.cube");
 	if (!file.is_open()) {
 		std::cerr << "ERROR: Faild to save CUBE LUT.\n";
 		return;
 	}
-
+	const size_t lut_size = get_lut_size<float>();
+	
 	// Write the metadata header.
 	file << "# Created by CMSLUTGenerator\n";
-	file << "LUT_3D_SIZE " << g_config.lut_size << "\n";
+	file << "LUT_3D_SIZE " << lut_size << "\n";
 	file << "DOMAIN_MIN 0.0 0.0 0.0\n";
 	file << "DOMAIN_MAX 1.0 1.0 1.0\n";
 	file << "\n";
 
 	// Write the normalized RGB table.
 	size_t i = 0;
-	while (i < cube(g_config.lut_size) * 4) {
-		file << std::fixed << std::setprecision(6) << static_cast<float>(lut[i++]) / 65535.0f << " ";
-		file << std::fixed << std::setprecision(6) << static_cast<float>(lut[i++]) / 65535.0f << " ";
-		file << std::fixed << std::setprecision(6) << static_cast<float>(lut[i++]) / 65535.0f << "\n";
-		i++; // Iterate over alpha.
+	while (i < cube(lut_size) * 3) {
+		file << std::fixed << std::setprecision(6) << lut[i++] << " ";
+		file << std::fixed << std::setprecision(6) << lut[i++] << " ";
+		file << std::fixed << std::setprecision(6) << lut[i++] << "\n";
 	}
 
 	file.close();
@@ -172,12 +191,13 @@ static void write_lut_to_cube_file(const std::vector<uint16_t>& lut)
 static void write_lut_to_dds_file(std::vector<uint8_t>& lut)
 {
 	// We need to manualy create an array of 2D slices from the LUT data.
-	std::vector<DirectX::Image> images(g_config.lut_size);
-	const size_t row_pitch = g_config.lut_size * 4; // width * n channels * bytes per channel
-	const size_t slice_pitch = row_pitch * g_config.lut_size; // row pitch * height
-	for (size_t i = 0; i < g_config.lut_size; ++i) {
-		images[i].width = g_config.lut_size;
-		images[i].height = g_config.lut_size;
+	const size_t lut_size = get_lut_size<uint8_t>();
+	std::vector<DirectX::Image> images(lut_size);
+	const size_t row_pitch = lut_size * 4; // width * n channels * bytes per channel
+	const size_t slice_pitch = row_pitch * lut_size; // row pitch * height
+	for (size_t i = 0; i < lut_size; ++i) {
+		images[i].width = lut_size;
+		images[i].height = lut_size;
 		images[i].format = DXGI_FORMAT_R8G8B8A8_UNORM;
 		images[i].rowPitch = row_pitch;
 		images[i].slicePitch = slice_pitch;
@@ -261,16 +281,16 @@ int main(int argc, char** argv)
 	//
 
 	// Create and tranform LUTs.
-	std::vector<uint8_t> lut8(cube(g_config.lut_size) * 4);
-	std::vector<uint16_t> lut16(cube(g_config.lut_size) * 4);
-	if (!transform_luts(lut8, lut16)) {
+	std::vector<uint8_t> lut_rgba8(cube(get_lut_size<uint8_t>()) * 4);
+	std::vector<float> lut_rgb32f(cube(get_lut_size<float>()) * 3);
+	if (!transform_luts(lut_rgba8, lut_rgb32f)) {
 		std::cerr << "ERROR: Faild to create LUTs.\n";
 		return 1;
 	}
 
 	// Save LUTs.
-	write_lut_to_cube_file(lut16);
-	write_lut_to_dds_file(lut8);
+	write_lut_to_cube_file(lut_rgb32f);
+	write_lut_to_dds_file(lut_rgba8);
 
 	return 0;
 }
