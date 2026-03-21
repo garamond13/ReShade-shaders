db3eb73540bcb6081e942d5af7b5ce55a94a9df7
garamond13 <98652255+garamond13@users.noreply.github.com>
2026-03-21 19:51:56 +0100

    BioShockInfiniteGraphicalUpgrade: Increase bloom radius
    Increase bloom radius to better match the original bloom radius.

--- Addons/BioShockInfiniteGraphicalUpgrade/src/BioShockInfiniteGraphicalUpgrade/main.cpp
+++ Addons/BioShockInfiniteGraphicalUpgrade/src/BioShockInfiniteGraphicalUpgrade/main.cpp
@@ -1542,7 +1542,7 @@ static void draw_settings_overlay(reshade::api::effect_runtime* runtime)
 }
 
 extern "C" __declspec(dllexport) const char* NAME = "BioShockInfiniteGraphicalUpgrade";
-extern "C" __declspec(dllexport) const char* DESCRIPTION = "BioShockInfiniteGraphicalUpgrade v2.2.0";
+extern "C" __declspec(dllexport) const char* DESCRIPTION = "BioShockInfiniteGraphicalUpgrade v2.3.0";
 extern "C" __declspec(dllexport) const char* WEBSITE = "https://github.com/garamond13/ReShade-shaders/tree/main/Addons/BioShockInfiniteGraphicalUpgrade";
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD fdwReason, LPVOID)
@@ -1566,9 +1566,9 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD fdwReason, LPVOID)
 			g_srv_bloom_mips_x.resize(g_bloom_nmips);
 			g_bloom_sigmas.resize(g_bloom_nmips);
 			g_bloom_sigmas[0] = 1.5f;
-			g_bloom_sigmas[1] = 1.0f;
-			g_bloom_sigmas[2] = 1.0f;
-			g_bloom_sigmas[3] = 1.0f;
+			g_bloom_sigmas[1] = 2.0f;
+			g_bloom_sigmas[2] = 2.0f;
+			g_bloom_sigmas[3] = 2.0f;
 			g_bloom_sigmas[4] = 1.0f;
 			g_bloom_sigmas[5] = 1.0f;
 
